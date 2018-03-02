09a376780e30703ba7df4229c7fa238e0d69ea0e
Yonemasa <yonekura@ion-ep.com>
2018-03-02 10:55:31 +0900

    Update index.php

--- web/index.php
+++ web/index.php
@@ -34,9 +34,9 @@ $app->post('/callback', function (Request $request) use ($app) {
 	$myPath = str_replace("index.php", "", __FILE__);
 
 	$data01 = "\r\n\r\n--------------------- data desu\r\n\r\n";
-//	foreach ($body['entry'][0]['changes'][0]['value'] as $key => $val) {
-//		$data01 .= $key . ":" . $val . "\r\n";
-//	}
+	foreach ($body['entry'][0]['changes'][0]['value'] as $key => $val) {
+		$data01 .= $key . ":" . $val . "\r\n";
+	}
 	$data01 .= multi_implode($body, "\r\n");
 	
 	if (str_pos($data01, "instagram") === false) {
