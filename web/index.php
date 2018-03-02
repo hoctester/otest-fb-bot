efc59e9dfb28fbd40abae2241753607a233e7302
Yonemasa <yonekura@ion-ep.com>
2018-03-02 11:41:19 +0900

    Update index.php

--- web/index.php
+++ web/index.php
@@ -40,7 +40,8 @@ $app->post('/callback', function (Request $request) use ($app) {
 	$data01 .= multi_implode($body, "\r\n");
 	
 	if (strpos($data01, "instagram") === false) {
-		$data01 = "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\r\n";
+		$data01 = "";
+//		$data01 = "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\r\n";
 	}
 		
 //	$txt = date("Y-m-d H:i:s") . "  " . $text . "\r\n";
